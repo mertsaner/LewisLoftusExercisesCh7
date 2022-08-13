c229d1b8cb052c8b728e645a5f32786bbafb2a72
mert.saner <mert.saner@campus.tu-berlin.de>
2022-08-13 11:11:33 +0200

    P7.6 Done!

--- src/Task.java
+++ src/Task.java
@@ -1,43 +1,51 @@
 /** JAVA LEWIS & LOFTUS EX 7.5
 *  @author mertsaner
 * I will add many objects to this class, like I did on my to-do-list
-
 * */
 public class Task implements Priority{
    // public static int taskPriority;  //Task class represents to-do-list, not possible to use
    // since it changes all instances of the class
 
     private String taskName;
+    private int complexityLevel;
     public int taskPriority;
-    // We commented this and defined taskPriority as Static to use it in static class(main), but it didn't worked out since;
-//Static variables are shared across instances of the class. When edited, they change for all instances
+
+    //We commented this and defined taskPriority as Static to use it in static class(main), but it didn't worked out since;
+    //Static variables are shared across instances of the class. When edited, they change for all instances
     private String description;
 
     //Constructor, that will initialize variables for Task class
     //Each task should have its name as well as priority weight
-    public Task (String taskName, int taskPriority){
+    public Task (String taskName, int taskPriority, int complexityLevel){
         this.taskName = taskName;
-        this.taskPriority =taskPriority;
-    }
-
-    public void setTaskPriority(int taskPriority) { //important to assign task priority
         this.taskPriority = taskPriority;
+        this.complexityLevel = complexityLevel;
     }
-
-    public void setTaskName(String taskName) { //optional set to change task name
-        this.taskName = taskName;
+    //====
+    //Complexity Interface Accessors and Mutators
+    public int getComplexityLevel() {return complexityLevel;}
+    public void setComplexityLevel(int complexityLevel){
+        this.complexityLevel = complexityLevel;
     }
-
+    //====
+    //TaskPriority Accessors and Mutators
     public int getTaskPriority() {
         return taskPriority;
     }
-
-    public String getTaskName() {
-        return taskName;
+    public void setTaskPriority(int taskPriority) { //important to assign task priority
+        this.taskPriority = taskPriority;
     }
+    //====
+    //TaskName Accessors and Mutators
+    public void setTaskName(String taskName) { //optional setter method to change task name
+        this.taskName = taskName;
+    }
+    public String getTaskName() {return taskName;}
+    //====
 
     public String toString() {
-        String result = "Item: " + taskName + ", Priority(lower is higher priority): " + Integer.toString(taskPriority);
+        String result = "Item: " + taskName + ", Priority(lower is higher priority): " + Integer.toString(taskPriority)
+                + ", ComplexityLevel: " + getComplexityLevel()  ;
         return result;
     }
 }
--- src/TaskTester.java
+++ src/TaskTester.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+import java.util.Arrays;
 /** For instance, shopping items, laundry, washing dishes, programming,
         * Going out, sunbathing, go to gym, go for a walk
         * The program will priorities this list according my needs and then print the final prioritized list
@@ -6,12 +7,12 @@ import java.util.Date;
  public class TaskTester {
     public static void main (String[] args ){       //DRIVER CLASS OF TASK CLASS
 
-        Task Edeka = new Task("Shopping in Edeka", 3);
-        Task Laundry = new Task("Laundry", 7);
-        Task ZIB = new Task("Working for ZIB", 2);
-        Task Gym = new Task("Going to Gym", 4);
-        Task Java = new Task("Studying Java", 1);
-        Task Sunbathing = new Task("Sunbathing", 5);
+        Task Edeka = new Task("Shopping in Edeka", 3, 0);
+        Task Laundry = new Task("Laundry", 7, 0);
+        Task ZIB = new Task("Working for ZIB", 2, 5);
+        Task Gym = new Task("Going to Gym", 4, 1);
+        Task Java = new Task("Studying Java", 1, 8);
+        Task Sunbathing = new Task("Sunbathing", 5, 0);
 
         Date d = new Date();
         System.out.println(Edeka);
@@ -31,8 +32,6 @@ import java.util.Date;
 
         // object1.someMethod();       //Called the method of the another class by creating its object
 
-        System.out.println(Edeka.taskPriority);
-
         switch (dayString) {        //Task.taskPriority
             case "Mon":
                 //Make it in a way that the item which has priority 1 will be replaced to 2 second each priority will be step below
@@ -43,20 +42,18 @@ import java.util.Date;
             case "Wed":
                 break;
             case "Thu":
-                ZIB.setTaskPriority(1);
                 break;
             case "Fri":
+                ZIB.setTaskPriority(1);
                 break;
             case "Sat":
                 break;
             case "Sun":
                 System.out.println("Entered!");
-                for (int i = 1; i < 10 ; i++){
-                        if(Task.taskPriority == 1)
-                        {}
-                }
-                System.out.println(ZIB.taskPriority);
-
+//                for (int i = 1; i < 10 ; i++){
+//                        if(Task.taskPriority == 1)
+//                        {}
+//                }
                 ZIB.setTaskPriority(1);
                 break;
             default:
@@ -64,7 +61,12 @@ import java.util.Date;
                 break;
         }
 
+        System.out.println("ZIB task has changed, it is urgent now and includes MIP optimization");
+        ZIB.setTaskPriority(1);
+        ZIB.setComplexityLevel(9);
+
         System.out.println("===================================");
+        System.out.println("ZIB task has changed, it is urgent now and includes MIP optimization");
         System.out.println("NEW RESULTS!");
         System.out.println("===================================");
         //Print Again
